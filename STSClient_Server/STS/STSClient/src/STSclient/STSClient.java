769d2b3a8ce88f809b63381cbb5ec95f0049e1bf
Eranus <ecthomps@umich.edu>
2017-10-03 15:35:44 -0400

    establish ClientServer Connection

---
+++ STSClient_Server/STS/STSClient/src/STSclient/STSClient.java
@@ -0,0 +1,60 @@
+package STSclient;
+
+import java.io.*;
+import java.net.*;
+
+/*
+	@author Joel&Eranus
+*/
+class STSClient
+{
+    public static void main(String argv[]) throws Exception
+    {
+        String userInput= "";
+        String serverResponce= "";
+        String endCmd = "-1", nullInput = "";
+
+        //create inputStream
+        BufferedReader inputFromUser = new BufferedReader(new InputStreamReader(System.in));
+        //create clientSocket
+        Socket clientSocket = new Socket("localhost", 6789);
+        //create outputStream to server
+        DataOutputStream outputToServer = new DataOutputStream(clientSocket.getOutputStream());
+        //establish clientServerConnection
+        BufferedReader inputFromServer = new BufferedReader
+                                        (new InputStreamReader(clientSocket.getInputStream()));
+
+        printCommand(clientSocket);
+
+        while (true) {
+            System.out.print("Command: ");
+            userInput = inputFromUser.readLine(); 
+            outputToServer.writeBytes(userInput + '\n');
+
+            String[] splitted = userInput.trim().split(" ");
+            
+        if(endCmd.equals(userInput) || nullInput.equals(userInput)){
+            
+            break;
+                }else
+            for(int i = Integer.parseInt(splitted[1]); i <= Integer.parseInt(splitted[2]); i++){
+                serverResponce = inputFromServer.readLine();
+                System.out.println(serverResponce);
+            }
+        } 
+       
+        clientSocket.close();
+        
+    }
+
+    public static void printCommand(Socket clientSocket) {
+
+        if (clientSocket.isConnected()) {
+            System.out.println("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+            System.out.println("+-+-+-+-+-+-+   Simple TCP Service (STS)  +-+-+-+-+-+-+");
+            System.out.println("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+            System.out.println("Commands allowed by the server for this client:");
+            System.out.println("\t\tdownload [starting line #] [ending line #]");
+        }
+    }
+}
---
+++ STSClient_Server/STS/STSServer/src/STSserver/STSServer.java
@@ -0,0 +1,52 @@
+package STSserver;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.net.*;
+import java.nio.*;
+import java.util.*;
+import java.util.stream.*;
+
+
+/*
+	@author Joel&Eranus
+*/
+class STSServer
+{
+    public static void main(String[] args) throws IOException {
+        String clientResponce = "";
+        int charTrack = 0;
+        String responce = "";
+
+        //create socket at port 6789
+        ServerSocket welcomeSocket = new ServerSocket(6789);
+        Socket connectionSocket = welcomeSocket.accept();
+        //establish clientServer connection
+        BufferedReader inputFromClient = new BufferedReader
+                (new InputStreamReader(connectionSocket.getInputStream()));
+        //create outputSteam to client
+        DataOutputStream outputToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+        while (true) {
+            clientResponce = inputFromClient.readLine();
+            String[] splitted = clientResponce.trim().split(" ");
+            System.out.println("Client Response: " + clientResponce);
+
+            for (int i = Integer.parseInt(splitted[1]); i <= Integer.parseInt(splitted[2]); i++) {
+                try (Stream<String> lines = Files.lines(Paths.get("alice.txt"))) {
+                    responce = lines.skip(i).findFirst().get() + "\n";
+                    responce = i + " " + charTrack + " " + responce;
+                    //two spaces, so we subtract 2.
+                    charTrack += responce.length() - String.valueOf(i).length() - String.valueOf(charTrack).length() - 2;
+                    outputToClient.writeBytes(responce.toUpperCase());
+                    System.out.println("TO CLIENT: " + responce.toUpperCase());
+                }
+            }
+            charTrack = 0;
+            System.out.println("Done");
+        }
+    }
+
+}
+
